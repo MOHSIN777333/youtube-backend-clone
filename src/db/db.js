f920b3074039d80d33425ecf2d853c845cd5409e
MOHSIN <mohsinali031332@example.com>
2025-12-16 01:41:22 -0800

    connection mongodb atals

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `${process.env.MONGODB_URI}/ ${DB_NAME}`
+    );
+    console.log(`successfully connect mongodb server_host:${conn.connection.host}`);
+  } catch (error) {
+    console.log(`mongodb connection is faild :`, error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
