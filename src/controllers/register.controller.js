a097be5f799b15fb9812002d9fa91dfccd8ed5d7
MOHSIN <mohsinali031332@example.com>
2025-12-26 18:42:29 +0500

    just write a controller

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,9 +1,34 @@
-import { asyncHandler } from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
 
-const userRegister = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message:"ok"
-    })
-})
+const userRegister = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  //check if user already exists: username , email
+  //check for images, check avatar
+  // create user object - create entry in db
+  //remove password and refresh toten feild from response
+  //chect for user creation
+  // return response
+  const { username, fullName, email, password } = req.body;
 
-export {userRegister}
+  console.log("check feild", email, username, fullName, password);
+
+  if (
+    [username, fullName, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all field are required ");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if(existedUser){
+    throw new ApiError(409, "email or username is already existed")
+  }
+  
+});
+
+export { userRegister };
